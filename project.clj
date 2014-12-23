35dbac938021240fbdcbdbc51f8f0cb1adbccda3
frankiesardo <fran.sardo@gmail.com>
2014-12-23 22:04:21 +0000

    Automate codox

--- project.clj
+++ project.clj
@@ -9,4 +9,5 @@
                  [prismatic/schema "0.3.2"]
                  [metosin/ring-swagger "0.12.0"]
                  [metosin/ring-swagger-ui "2.0.17"]]
+  :codox {:src-dir-uri "http://github.com/frankiesardo/pedestal-swagger/blob/master/"}
   :profiles {:dev {:dependencies [[io.pedestal/pedestal.jetty "0.1.5"]]}})
