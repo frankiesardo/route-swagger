dc59e7f80c14f576529a81dedd6ec156163fe285
frankiesardo <fran.sardo@gmail.com>
2015-03-08 10:07:39 +0000

    Up sample project version

--- sample/project.clj
+++ sample/project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [frankiesardo/pedestal-swagger "0.2.0-SNAPSHOT"]
+                 [frankiesardo/pedestal-swagger "0.3.1-SNAPSHOT"]
 
                  [io.pedestal/pedestal.service "0.3.0"]
                  [io.pedestal/pedestal.jetty "0.3.0"]
