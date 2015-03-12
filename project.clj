137202a4efe84662b18cab39539da376bb0cc534
Frankie Sardo <fran.sardo@gmail.com>
2015-03-12 20:49:54 +0000

    Either return or print. Not both

--- project.clj
+++ project.clj
@@ -8,6 +8,8 @@
                  [io.pedestal/pedestal.service "0.3.0"]
                  [io.pedestal/pedestal.jetty "0.3.0"]
 
+
+
                  [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.7"]
                  [org.slf4j/jcl-over-slf4j "1.7.7"]
