308d6b6a818a06ca97bae0a382301a77a22cd5ee
Francesco Sardo <fran.sardo@gmail.com>
2015-03-25 11:43:35 +0000

    Polishing up build tools

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [frankiesardo/pedestal-swagger "0.3.1-SNAPSHOT"]
-                 [io.pedestal/pedestal.service "0.3.0"]
+                 [frankiesardo/pedestal-swagger "LATEST"]
                  [io.pedestal/pedestal.jetty "0.3.0"]
                  [ch.qos.logback/logback-classic "1.1.2"
                   :exclusions [org.slf4j/slf4j-api]]
