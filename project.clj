7c02a97bf709d72220688a50ffaff1200ecb95ed
Francesco Sardo <fran.sardo@gmail.com>
2015-04-30 08:41:46 +0000

    Merge pull request #9 from nubank/master
    Pedestal 0.4.0

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [frankiesardo/pedestal-swagger "0.3.1-SNAPSHOT"]
-                 [io.pedestal/pedestal.jetty "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.2"
-                  :exclusions [org.slf4j/slf4j-api]]
+                 [frankiesardo/pedestal-swagger "0.4.0-SNAPSHOT"]
+                 [io.pedestal/pedestal.service "0.4.0"]
+                 [io.pedestal/pedestal.jetty "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.7"]
                  [org.slf4j/jcl-over-slf4j "1.7.7"]
                  [org.slf4j/log4j-over-slf4j "1.7.7"]]
+  :main ^{:skip-aot true} sample.server
   :uberjar-name "sample-standalone.jar"
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
