034efa811fe58ebb43e91f4b23b935ead9ad2edf
Frankie Sardo <fran.sardo@gmail.com>
2015-07-23 01:11:00 +0100

    Version 0.4.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject frankiesardo/pedestal-swagger "0.4.1"
+(defproject frankiesardo/pedestal-swagger "0.4.2-SNAPSHOT"
   :description "Swagger documentation for Pedestal routes"
   :url "http://github.com/frankiesardo/pedestal-swagger"
   :license {:name "Eclipse Public License"
