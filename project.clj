44dcb289680a4bf349b5b240d7d944e1731d949a
Edward Wible <edward.wible@nubank.com.br>
2017-01-14 18:11:39 -0200

    Bump to pedestal 0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nu-pedestal-swagger "0.4.4-SNAPSHOT"
+(defproject nu-pedestal-swagger "0.4.5-SNAPSHOT"
   :description "Swagger documentation for Pedestal routes"
   :url "http://github.com/frankiesardo/pedestal-swagger"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
   :codox {:src-dir-uri "http://github.com/frankiesardo/pedestal-swagger/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.pedestal/pedestal.service "0.5.1"]
+                 [io.pedestal/pedestal.service "0.5.2"]
                  [metosin/ring-swagger "0.22.4"]
                  [metosin/ring-swagger-ui "2.1.4-0"]
                  [frankiesardo/linked "1.2.2"]
@@ -19,5 +19,5 @@
                  ["central" {:url "http://repo1.maven.org/maven2/" :snapshots false}]
                  ["clojars" {:url "https://clojars.org/repo/"}]]
 
-  :profiles {:dev {:dependencies [[io.pedestal/pedestal.jetty "0.5.1"]
+  :profiles {:dev {:dependencies [[io.pedestal/pedestal.jetty "0.5.2"]
                                   [metosin/scjsv "0.2.0" :exclusions [org.clojure/core.async]]]}})
